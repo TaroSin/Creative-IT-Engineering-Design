26ea96ea1f5297bb3c8cda679b8c62ff9827f500
MH-LEE <dlalsgur7711@naver.com>
2020-06-16 19:25:56 +0900

    modify after comment

--- Calendar.cpp
+++ Calendar.cpp
@@ -11,7 +11,7 @@ int getweek(int year, int month);
 void Cal_leap(); // 윤년계산 
 void output_calendar(); // 입력받은 연도와 월의 달력을 출력  
 
-int main(void)
+int main()
 {
 	input();
     week = getweek(year, month);    
@@ -41,6 +41,8 @@ int main(void)
     	output_calendar();   	
 	}
 	else cout << "Error!" << endl;
+	
+	return 0;
 }
  
 void input(){
@@ -66,47 +68,6 @@ void Cal_leap(){
 
 void output_calendar(){
 	cout << "  " << endl << "       " << year << "년 " << month << "월       " << endl << endl << "  " <<
-        "일 " << "월 " << "화 " << "수 " << "목 " << "금 " << "토  " << endl << "  ";
-
-    for (int i = 0; i < week; i++)
-        cout << "   ";
-
-    for (int j = 1; j <= Month_days[month - 1]; j++)
-    {
-        printf("%2d ", j);
-        if (week % 7 == 6)
-            cout << "  " << endl << "  ";
-        week = (week + 1) % 7;
-    }
-    cout << endl;
-}
-
-=======
-
-int getweek(int year, int month){
-    int week = year + (year / 4 - year / 100 + year / 400);
-
-    for (int i = 0; i < month - 1; i++)
-        week += Month_days[i];
-
-    return (week % 7);
-}
-
-int main(void)
-{
-    int year, month, week;
-
-    printf("년: ");
-    cin >> year;
-    printf("월: ");
-    cin >> month;
-
-    if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
-        Month_days[1]++;
-
-    week = getweek(year, month);
-
-    cout << "  " << endl << "       " << year << "년 " << month << "월       " << endl << endl << "  " <<
         "일 " << "월 " << "화 " << "수 " << "목 " << "금 " << "토  " << endl << "  ";
 
     for (int i = 0; i < week; i++)

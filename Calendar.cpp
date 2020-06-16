fde952210bd6cd630223ed04a511c6fe977677f7
MH-LEE <dlalsgur7711@naver.com>
2020-06-16 16:19:36 +0900

    function realization

--- Calendar.cpp
+++ Calendar.cpp
@@ -4,6 +4,50 @@
 using namespace std;
 
 int Month_days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+int year, month, week;
+
+void input(); // 연도와 월을 입력받는 함수. 
+int getweek(int year, int month); 
+void Cal_leap(); // 윤년계산 
+void output_calendar(); // 달력  출 력  
+
+int main(void)
+{
+
+	input();
+    week = getweek(year, month);    
+    Cal_leap();
+    output_calendar();
+    
+    int choice_num;
+    char ans;
+    
+    cout << "\n\n" << "[원하는 옵션 선택]" << "\n" << endl;
+    cout << "① 일정 추가" << endl; 
+    cout << "② 일정 수정" << endl; 
+    cout << "③ 일정 삭제" << endl; 
+    cout << "④ 음력 변환" << endl; 
+    cout << "⑤ 날짜 계산" << endl; 
+    cout << "⑥ 전체 일정 확인" << endl; 
+    cout << "⑦ 날짜 변경" << endl << endl;
+    
+    cout << "옵션 변호: "; 
+    cin >> choice_num; 
+    if(choice_num == 7){
+    	cout << endl;
+    	input();
+    	week = getweek(year, month);    
+    	Cal_leap();
+    	output_calendar();   	
+	}
+}
+ 
+void input(){
+	printf("년: ");
+    cin >> year;
+    printf("월: ");
+    cin >> month;
+}
 
 int getweek(int year, int month){
     int week = year + (year / 4 - year / 100 + year / 400);
@@ -14,21 +58,13 @@ int getweek(int year, int month){
     return (week % 7);
 }
 
-int main(void)
-{
-    int year, month, week;
+void Cal_leap(){
+	if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
+    Month_days[1]++;
+}
 
-    printf("년: ");
-    cin >> year;
-    printf("월: ");
-    cin >> month;
-
-    if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
-        Month_days[1]++;
-
-    week = getweek(year, month);
-
-    cout << "  " << endl << "       " << year << "년 " << month << "월       " << endl << endl << "  " <<
+void output_calendar(){
+	cout << "  " << endl << "       " << year << "년 " << month << "월       " << endl << endl << "  " <<
         "일 " << "월 " << "화 " << "수 " << "목 " << "금 " << "토  " << endl << "  ";
 
     for (int i = 0; i < week; i++)

8a054080c73bd2085a2ae435ef2b5b9708fdd7c1
kbohang0407 <kbohang04@naver.com>
2020-06-25 14:35:41 +0900

    ADD DDAY

--- dday.cpp
+++ dday.cpp
@@ -32,6 +32,7 @@ void DayofYear::SetToday(int tm, int td)
 	t_day = td;
 }
 
+// m월의 날짜 수
 
 void DayofYear::SetToday(int m, int d)
 {
@@ -45,17 +46,20 @@ int DayofYear::GetDays(int m)
 	return Month_days[m - 1];
 }
 
+// 1월 1일부터 m월 d일까지 날짜 합
 
 int DayofYear::GetPastDay(int m, int d)
 {
 	int i, past = 0;
 
+	// 1월부터 바로 전달까지 날짜 합
 
 	for (i = 1; i < m; i++)
 	{
 		past = past + GetDays(i);
 	}
 
+	// 이번달 날짜 합
 
 	past = past + d;
 
@@ -64,6 +68,7 @@ int DayofYear::GetPastDay(int m, int d)
 
 int DayofYear::GetLeftDay()
 {
+	// 1월 1일부터 D-day까지의 날짜 합 - 1월 1일부터 오늘까지의 날짜 합
 
 	return GetPastDay(d_mon, d_day) - GetPastDay(t_mon, t_day);
 }
@@ -72,6 +77,7 @@ void DayofYear::ShowLeftDay()
 {
 	int left = GetLeftDay();
 
+	// D-day가 오늘 날짜보다 앞설 경우
 	if (left < 0)
 	{
 		cout << "D-day가 오늘 날짜보다 앞설 수 없음" << endl;
